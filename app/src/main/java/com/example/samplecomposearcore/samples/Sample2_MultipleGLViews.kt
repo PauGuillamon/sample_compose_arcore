4fca78fe4b896082607e9f292cae726afb44eb2d
Pau Guillamon <pauguillamonjaime@gmail.com>
2024-07-02 10:10:45 +0200

    Adds description inside Sample2

--- app/src/main/java/com/example/samplecomposearcore/samples/Sample2_MultipleGLViews.kt
+++ app/src/main/java/com/example/samplecomposearcore/samples/Sample2_MultipleGLViews.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,9 +20,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.samplecomposearcore.R
 import com.example.samplecomposearcore.compose.GLThreadedRenderer
 import com.example.samplecomposearcore.compose.OpenGLView
 import com.example.samplecomposearcore.ui.theme.SampleComposeARCoreTheme
@@ -34,14 +38,17 @@ fun Sample2_MultipleGLViews() {
     Surface(
         modifier = Modifier.fillMaxSize(),
     ) {
+        val scrollState = rememberScrollState()
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(scrollState),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+
         ) {
             Text(
-                text = "This is a title",
+                text = stringResource(id = R.string.sample2_text_1),
                 modifier = Modifier
                     .padding(16.dp),
                 textAlign = TextAlign.Center,
@@ -54,7 +61,7 @@ fun Sample2_MultipleGLViews() {
                     .clip(CutCornerShape(16.dp))
             )
             Text(
-                text = "Text in between",
+                text = stringResource(id = R.string.sample2_text_2),
                 modifier = Modifier.padding(16.dp),
                 textAlign = TextAlign.Center,
             )
@@ -66,7 +73,7 @@ fun Sample2_MultipleGLViews() {
                     .clip(RoundedCornerShape(16.dp))
             )
             Text(
-                text = "Text at the end",
+                text = stringResource(id = R.string.sample2_text_3),
                 modifier = Modifier.padding(16.dp),
                 textAlign = TextAlign.Center,
             )
